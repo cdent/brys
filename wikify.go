a8f31384b060f307f5930506a244f300a2c5389f
Chris Dent <cdent@anticdent.org>
2019-11-14 18:31:28 +0000

    Fix multi-WikiWord in one string rendering
    If there were two WikiWord NextTo one another, the second would
    not render. Now it does. Improved tests to check this.

--- wikify.go
+++ wikify.go
@@ -6,7 +6,7 @@ import (
 
 // Simple WikiWord, for now, we can't use \b because of unicode
 // and golang \b is ascii only (which is fair)
-var wikiWord = regexp.MustCompile(`(\A|\s)(\p{Lu}\p{Ll}+\p{Lu}\p{Ll}+)(\p{P}|\s|\z)`)
+var wikiWord = regexp.MustCompile(`(\A|\p{P}|\s)?(\p{Lu}\p{Ll}+\p{Lu}\p{Ll}+)(\p{P}|\s|\z)`)
 
 func wikify(c string) string {
 	return wikiWord.ReplaceAllString(c, "$1<a class=\"wikiword\" href=\"/p/$2\">$2</a>$3")
--- wikify_test.go
+++ wikify_test.go
@@ -18,3 +18,20 @@ func TestWikify(t *testing.T) {
 		}
 	}
 }
+
+func TestFullStringWikify(t *testing.T) {
+	lines := [][]string{
+		{"We have OneWord and TwoWord in this line.", "We have <a class=\"wikiword\" href=\"/p/OneWord\">OneWord</a> and <a class=\"wikiword\" href=\"/p/TwoWord\">TwoWord</a> in this line."},
+		{"Now OneWord TwoWord next to one another.", "Now <a class=\"wikiword\" href=\"/p/OneWord\">OneWord</a> <a class=\"wikiword\" href=\"/p/TwoWord\">TwoWord</a> next to one another."},
+		{"OneWord TwoWord at the start.", "<a class=\"wikiword\" href=\"/p/OneWord\">OneWord</a> <a class=\"wikiword\" href=\"/p/TwoWord\">TwoWord</a> at the start."},
+		{"At the end is OneWord TwoWord", "At the end is <a class=\"wikiword\" href=\"/p/OneWord\">OneWord</a> <a class=\"wikiword\" href=\"/p/TwoWord\">TwoWord</a>"},
+		{"At the end is OneWord TwoWord.", "At the end is <a class=\"wikiword\" href=\"/p/OneWord\">OneWord</a> <a class=\"wikiword\" href=\"/p/TwoWord\">TwoWord</a>."},
+	}
+
+	for _, line := range lines {
+		wiki := wikify(line[0])
+		if wiki != line[1] {
+			t.Errorf("\"%s\" wikified to \"%s\", expected \"%s\"", line[0], wiki, line[1])
+		}
+	}
+}
